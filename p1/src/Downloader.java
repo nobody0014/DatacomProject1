c1f244fccfcac029f00cf3649d7f175c30f4bc25
nobody0014 <wittz0014@gmail.com>
2016-10-07 11:29:52 +0700

    Download now can connect and timeout, srget will be the one calling download, srget can seperate proper input

--- p1/src/Downloader.java
+++ p1/src/Downloader.java
@@ -1,4 +1,38 @@
-
+import java.util.*;
+import java.io.*;
+import java.net.*;
 public class Downloader {
-
+	
+	//global var that we will use
+	Socket client;	
+	String servName;
+	
+	//standard http port is 80
+	int port = 80;
+	
+	//Constructors
+	public Downloader(String servName){
+		this.servName = servName;
+	}
+	public Downloader(String servName, int port){
+		this(servName);
+		this.port = port;
+	}
+	
+	//Make new socket and connect it, time out if doesnt work
+	public void connect(){
+		try{
+			client = new Socket();
+			client.connect(new InetSocketAddress(servName, port), 10000);
+			System.out.println("Connection established");
+		}
+		catch (Exception e){
+			System.out.println("Unable to get response from server.\nQuiting");
+			System.exit(0);
+		}
+	}
+	public void close() throws IOException{
+		client.close();
+	}
+	
 }
--- p1/src/srget.java
+++ p1/src/srget.java
@@ -2,19 +2,32 @@ import java.util.*;
 import java.io.*;
 import java.net.*;
 public class srget {
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		//We are going to use main to check certain parts and throw args into new function
 		System.out.println(Arrays.toString(args));
-		if(args[0] != "-o"){
+		if(!args[0].equals("-o")){
 			System.out.println("Invalid Parameters");
 			System.exit(0);
 		}
 		else{
+			String[] destArr = args[args.length-1].split(":");
+			String servName = new String();
+			//port may not be initialised depending on the input
+			int port = 80;
+			if(destArr.length < 3){
+				servName = args[args.length-1];
+			}
+			else{
+				port = Integer.parseInt(destArr[1]);
+				servName += destArr[0] + destArr[2];
+			}
 			
+			//Create Downloader
+			Downloader d  = new Downloader(servName,port);
+			d.connect();
+			
+			
+			d.close();
 		}
-	}
-	//hub function is as the name implies.... a hub.
-	public static void hub(String[] args){
-		
 	}
 }
