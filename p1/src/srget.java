c5b2c6e2d20ee501873d47fc5ca507f2018b9bbb
nobody0014 <wittz0014@gmail.com>
2016-10-07 10:00:57 +0700

    Commiting entire project file

---
+++ p1/src/srget.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+public class srget {
+	public static void main(String[] args){
+		//We are going to use main to check certain parts and throw args into new function
+		System.out.println(Arrays.toString(args));
+		if(args[0] != "-o"){
+			System.out.println("Invalid Parameters");
+			System.exit(0);
+		}
+		else{
+			
+		}
+	}
+	//hub function is as the name implies.... a hub.
+	public static void hub(String[] args){
+		
+	}
+}
